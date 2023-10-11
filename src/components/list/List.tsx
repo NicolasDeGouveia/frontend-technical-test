3466d9742a9d2e4b38451b6fa25645726c2db7f4
unknown <nicolas.degouveia@gmail.com>
2023-10-11 18:48:00 +0200

    added conversations list

---
+++ src/components/list/List.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import { Conversation } from "../../types/conversation";
+
+type ListProps = {
+  conversation: Conversation;
+  userId: number;
+};
+
+const List = ({ conversation, userId }: ListProps) => {
+  // Convert the timestamp to a Date object
+  const date = new Date(conversation.lastMessageTimestamp * 1000);
+  console.log(userId);
+
+  // Get the month and day
+  const month = date.toLocaleString("en-US", { month: "short" }); // Change 'short' to 'long' for the full month name
+  const day = date.getDate();
+
+  return (
+    <div className="border border-black p-4 my-4 rounded-lg w-2/4 mx-auto">
+      <div className="flex flex-col">
+        <div className="font-bold">
+          {conversation.recipientId === userId
+            ? conversation.senderNickname
+            : conversation.recipientNickname}
+        </div>
+        <div className="mt-4">
+          {month} {day}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,36 @@
-import type { ReactElement } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import Logo from '../assets/lbc-logo.webp'
-import styles from '../styles/Home.module.css'
+import React, { useEffect, type ReactElement, useState } from "react";
+import { getLoggedUserId } from "../utils/getLoggedUserId";
+import List from "../components/list/List";
+import { Conversation } from "../types/conversation";
 
-const Home = ():ReactElement => {
-  const year = new Date().getFullYear()
+const Home = (): ReactElement => {
+  const [conversationsData, setConversationsData] = useState<Conversation[]>(
+    []
+  );
+  const user = getLoggedUserId();
+
+  useEffect(() => {
+    async function getConversationByUser(userId: number) {
+      const response = await fetch(
+        `http://localhost:3005/conversations/${userId}`
+      );
+      const data = await response.json();
+      setConversationsData(data);
+    }
+    getConversationByUser(user);
+  }, [user]);
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Frontend Technical test - Leboncoin</title>
-        <meta name="description" content="Frontend exercise for developpers who want to join us on leboncoin.fr"></meta>
-      </Head>
-
-      <main className={styles.main}>
-        <Image src={Logo} alt="Leboncoin Frontend Team" width={400} height={125} />
-        <h1 className={styles.title}>
-          Welcome !
-        </h1>
-
-        <p className={styles.description}>
-          This test is based on a <a title="Next.js documentation" href="https://nextjs.org/docs/getting-started" target="_blank" rel="noopener noreferrer">Next.js</a> application.<br />
-          Fork the repository and use the <code className={styles.code}>main</code> branch as your starting point.
-          <br /><br />
-
-          Get started by reading{' '}
-          <code className={styles.code}>README.md</code> and editing <code className={styles.code}>src/pages/index.js</code>
-          <br />
-          Once you are done, send the repository link to your HR contact.
-        </p>
-
-        <div className={styles.grid}>
-          <article className={styles.card}>
-            <h2>Design</h2>
-            <p>Feel free to create any design you want for this exercise. Let your creativity talks !</p>
-          </article>
-
-          <article className={styles.card}>
-            <h2>Libraries</h2>
-            <p>Feel free to use any library you want. Only Next.js / React are required.</p>
-          </article>
-
-          <article className={styles.card}>
-            <h2>API Server</h2>
-            <p>
-              Start the API server on port <code className={styles.code}>3005</code> by running<br /><code className={styles.code}>npm run start-server</code>.<br/>
-              Find the swagger definitions in <code className={styles.code}>docs/api-swagger.yml</code> or <a title="API Swagger documentation" href="https://leboncoin.tech/frontend-technical-test/" target="_blank" rel="noopener noreferrer">the online documentation</a>.
-            </p>
-          </article>
-
-          <article className={styles.card}>
-            <h2>Timing</h2>
-            <p>We recommend 4 hours for this test. You are free to spend more (or less) time, let us know how much time did you spend.</p>
-          </article>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        &copy; leboncoin - {year}
-      </footer>
+    <div className="max-w-[77rem] m-auto h-screen flex flex-col justify-center items-center">
+      {conversationsData.map((conversation: Conversation, index) => (
+        <React.Fragment
+          key={`${conversation.recipientId}-${conversation.senderId}`}
+        >
+          <List conversation={conversation} userId={user} />
+        </React.Fragment>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
